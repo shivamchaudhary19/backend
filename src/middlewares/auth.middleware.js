6baa975109a6ca69d7eb42f0ed66c9a4bdf3b501
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-09-22 19:55:40 +0530

    fixedlogout errors

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,10 +1,11 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async(req, res, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.reaplce("Bearer", "")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
     
         // if there's not token
         if (!token) {
@@ -12,7 +13,7 @@ export const verifyJWT = asyncHandler(async(req, res, next) => {
         }
     
         // if there's token
-        const decodedToken = jwt.verify(token, process.env.ACCCESS_TOKEN_SECRET)
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken") // why  _id ?, because when we created the model , we used _id
     
